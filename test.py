431db784b613d6e09605be638b6387582d840a13
XIANNAN717 <596609597@qq.com>
2020-06-03 21:41:14 +0800

    优化图片识别率

--- test.py
+++ test.py
@@ -1,6 +1,40 @@
-def aa():
-    print('调用aa方法')
-    return True
+from selenium.webdriver.common.by import By
+from public.po.base_view import BaseView
+import logging
+from log.log import logger
+from public.common.desired_caps import desired
+from public.common.do_excel import ReadExcel
+from public.po.login_page import OpenWinxin
+from public.common.adb_shell import AdbShell
+from public.po.new_order_page import NewOderPage
+from time import sleep
 
-if 1 and aa():
-    print(11)
+
+class TemporaryAuthorization(BaseView):
+    # 选择门店按钮
+    # shop_xpath = "//*[contains(text(),'测试门店001')]" # no
+    # shop_xpath = "//Android.widget.TextView[contains(@text,'测试门店001')]"
+    # shop_xpath = "//android.widget.TextView[@text='测试门店001']"
+    # shop_xpath = "//*[@class='android.webkit.WebView']/android.widget.View/android.widget.View"
+    # shop_xpath = "//*[@class='android.webkit.WebView']/View/View"
+    # shop_xpath = "//*[@resource-id='com.tencent.mm:id/kw']/../following-sibling::*[1]//android.widget.View/android.widget.View"
+    shop_xpath = "//*[@resource-id='com.tencent.mm:id/kw']/../following-sibling::*[1]//View/View"
+    # shop_xpath = "//*[@text='测试门店001']" # Y
+
+
+    shop_btn_element = (By.XPATH , shop_xpath)  # 临时授权按钮元素
+
+
+    # “选择门店”按钮
+    def shop_btn(self):
+        shop_btn = self.wait_find_element(*self.shop_btn_element)
+        shop_btn.click()
+
+
+if __name__ == '__main__':
+    driver = desired()
+    App = OpenWinxin(driver)
+    App.click_FD()
+    T = TemporaryAuthorization(driver)
+    T.shop_btn()
+
